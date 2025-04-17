d37f6c2223e9660095065f619a04b4652cd8c495
zhanx999 <17amina025@gmail.com>
2025-04-17 10:57:46 +0500

    сделала Auth (логика и стиль)

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,80 @@
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+import "./Auth.scss"
 
 const Auth = () => {
+    const [isLoggin, setIsLoggin] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [checkPassword, setCheckPassword] = useState('')
 
+    const clickAuthMode  = () => {
+        setIsLoggin((prev) => !prev)
+    }
+
+    useEffect (() => {
+        const authStatus = Cookies.get('authStatus');
+        if(authStatus === 'login'){
+            setIsLoggin(true);
+        } else {
+            setIsLoggin(false);
+        }
+    }, [])
+
+    const handleAuthAction = () => {
+        if (!email || !password) {
+            return;
+        }
+        if (!isLoggin && password !== checkPassword) {
+            return;
+        }
+        Cookies.set('authStatus', 'login')
+        Cookies.set('userEmail', email)
+        console.log('click button')
+    }
+    
+    return(
+        <div className="auth">
+            <h2>{ isLoggin ? "Вход в аккаунт" : "Регистрация" }</h2>
+
+            <div className="form">
+                <input 
+                    type="email" 
+                    placeholder="Логин"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    className="input_auth"
+                />
+
+                <input 
+                    type="password" 
+                    placeholder={ isLoggin ? "Пароль" : "Введите пароль"}
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    className="input_auth"
+                />
+                { !isLoggin && 
+                    <input 
+                        type="password" 
+                        placeholder="Подтвердите пароль"
+                        value={checkPassword}
+                        onChange={(e)=>setCheckPassword(e.target.value)}
+                        className="input_auth"
+                    /> }
+
+                <button onClick={handleAuthAction}>{isLoggin ? "Войти" : "Зарегистрироваться"}</button>
+
+                <div>
+                    {isLoggin ? (
+                        <p>Не имеете аккаунт? <a href="#" onClick={clickAuthMode}>Зарегистрироваться</a></p>
+                    ) : (
+                        <p>Имеете аккаунт? <a href="#" onClick={clickAuthMode}>Войти</a></p>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Auth;
