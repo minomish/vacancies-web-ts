49fba24514ceaf042795f5999a44ec4e5848cfef
zhanx999 <17amina025@gmail.com>
2025-04-17 16:36:59 +0500

    update logo

--- src/hook/useJobList.ts
+++ src/hook/useJobList.ts
@@ -2,7 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '../api/axiosInstance';
 import { User } from '../types/User';
 
-const fetchUsers = async (): Promise<User[]> => {
+//useQuery - помогает запрщивать данные и работать с их состоянием
+
+// создание асинхронной функции 
+const fetchUsers = async (/* параметр */): Promise<User[]> => { 
+    // async - возвращает Promise 
+    // Promise<User[]> - дает обещание что когда нибудь это функция вернет успешный результат 
+    // и он будет масивом типа User
     const response = await axiosInstance.get('?results=10'); 
     return response.data.results;
 };
